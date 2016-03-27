d3908b7aced190a64fddfeda417973bd87dc03e7
Frederic Culot <culot@FreeBSD.org>
2016-03-27 12:37:48 +0200

    Add keybindings

--- .emacs.d/etc/my-keybindings.el
+++ .emacs.d/etc/my-keybindings.el
@@ -5,4 +5,11 @@
 (global-set-key [M-left] 'tabbar-backward-tab)
 (global-set-key [M-right] 'tabbar-forward-tab)
 
+(global-unset-key (kbd "C-0"))
+(define-prefix-command 'my-prefix)
+(global-set-key (kbd "C-0") 'my-prefix)
+
+; code-related
+(define-key my-prefix (kbd "c c") 'compile)
+
 (provide 'my-keybindings)

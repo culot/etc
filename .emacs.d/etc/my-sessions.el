c99e90ed95904a5eed274ee1976a395672f88e86
Frederic Culot <culot@FreeBSD.org>
2016-03-27 16:02:37 +0200

    Can't find an appropriate session manager...

--- .emacs.d/etc/my-sessions.el
+++ .emacs.d/etc/my-sessions.el
@@ -1,6 +1,4 @@
-(require 'workgroups2)
-
-(setq wg-prefix-key (kbd "C-c s"))
-(setq wg-session-file (concat my-dir-bak "sessions"))
+(require 'desktop)
+(desktop-save-mode 1)
 
 (provide 'my-sessions)

eaa779c1d6f63b3a81c74f94af2c6d7e3c15090b
culot <culot@FreeBSD.org>
2016-08-19 15:42:28 +0200

    Hate those automatic new lines

--- .emacs.d/etc/my-c++.el
+++ .emacs.d/etc/my-c++.el
@@ -1,12 +1,13 @@
 (c-add-style "my-c++-style" 
 	     '("stroustrup"
-               (c-syntactic-indentation . 1)   ; automatic newline with {};
+               (c-syntactic-indentation . 1)   ; automatic indentation
 	       (indent-tabs-mode . nil)        ; use spaces rather than tabs
 	       (c-basic-offset . 2)))          ; indent by two spaces
 
 (defun my-c++-mode-hook ()
   (c-set-style "my-c++-style")
   (auto-fill-mode)
+  (c-auto-newline nil)          ; disable newline when hitting {};
   (c-toggle-auto-hungry-state 1))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)

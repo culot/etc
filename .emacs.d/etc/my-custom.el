ec0e062206633335a05b739b1736b9326c310064
culot <culot@FreeBSD.org>
2016-09-02 08:49:44 +0200

    Add setup for bookmarks

--- .emacs.d/etc/my-custom.el
+++ .emacs.d/etc/my-custom.el
@@ -3,4 +3,8 @@
 
 (setq org-agenda-files '("~/org"))
 
+(setq bookmark-save-flag 1) ; save bookmark file whenever it changes
+(setq bookmark-default-file (concat my-dir-var "bookmarks"))
+
+
 (provide 'my-custom)

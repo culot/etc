e7011a19dd4bd919af0144d904ae6870035b480f
culot <culot@FreeBSD.org>
2016-11-08 14:00:37 +0100

    Reorder items

--- .emacs.d/etc/my-keybindings.el
+++ .emacs.d/etc/my-keybindings.el
@@ -9,13 +9,13 @@
 ;(global-set-key [M-left] 'tabbar-backward-tab)
 ;(global-set-key [M-right] 'tabbar-forward-tab)
 
-;-------------------------------------------------- C-z prefix ---
+;-------------------------------------------------- M-q prefix ---
 (global-unset-key (kbd "M-q"))
 (define-prefix-command 'my-prefix)
 (global-set-key (kbd "M-q") 'my-prefix)
 (global-set-key (kbd "M-q M-q") 'fill-paragraph)
 
-;-------------------------------------- C-z prefix + single key---
+;-------------------------------------- M-q prefix + single key---
 (define-key my-prefix (kbd "w") 'ace-window)
 (define-key my-prefix (kbd "M-w") 'ace-window)
 (define-key my-prefix (kbd "l") 'redraw-display)
@@ -24,14 +24,14 @@
 (define-key my-prefix (kbd "c c") 'compile)
 (define-key my-prefix (kbd "c w") 'global-whitespace-mode)
 
-;--------------------------------------- text-related prefix-t ---
-(define-key my-prefix (kbd "t f") 'auto-fill-mode)
-(define-key my-prefix (kbd "t s") 'flyspell-mode)
-
 ;------------------------------------------- org-mode prefix-o ---
 (define-key my-prefix (kbd "o l") 'org-store-link)
 (define-key my-prefix (kbd "o a") 'org-agenda)
 (define-key my-prefix (kbd "o c") 'org-capture)
 (define-key my-prefix (kbd "o b") 'org-iswitchb)
+
+;--------------------------------------- text-related prefix-t ---
+(define-key my-prefix (kbd "t f") 'auto-fill-mode)
+(define-key my-prefix (kbd "t s") 'flyspell-mode)
 
 (provide 'my-keybindings)
